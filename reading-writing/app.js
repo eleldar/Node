18d315dc60dd0dc630fa2447850735bc3bbd3a30
eldar <eleldar@mail.ru>
2022-03-15 16:00:24 +0300

    manual CRUD

--- reading-writing/app.js
+++ reading-writing/app.js
@@ -7,21 +7,33 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
-});
-
-app.put('/products', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
-});
-
-app.delete('/products/:id', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
-});
-
 app.get('/products', (req, res) => {
-  // *РЕАЛИЗОВАТЬ*
+  res.json(products);
 })
+
+app.post('/products', function(req, res) {
+  const newProduct = { ...req.body, id: products.length + 1 }
+  products = [ ...products, newProduct]
+  res.json(newProduct);
+});
+
+app.put('/products', function(req, res) {
+  let updatedProduct;
+  products = products.map(p => {
+    if (p.id === req.body.id) {
+      updatedProduct = { ...p, ...req.body };
+      return updatedProduct;
+    }
+    return p;
+  })
+  res.json(updatedProduct);
+});
+
+app.delete('/products/:id', function(req, res) {
+  const deletedProduct = products.find(p => p.id === +req.params.id);
+  products = products.filter(p => p.id !== +req.params.id);
+  res.json(deletedProduct);
+});
 
 app.listen(port, () => console.log(`Прослушивание на порту ${port}!`))
   
