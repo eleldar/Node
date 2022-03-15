76b3f614e66bc3911ca7373356cdcfcb8b757e18
eldar <eleldar@mail.ru>
2022-03-15 13:34:53 +0300

    blank reading-writing

---
+++ reading-writing/client-delete-route.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const data = JSON.stringify({
+  id: 1
+})
+
+const options = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/products',
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const request = http.request(options, (res) => {
+  let body = '';
+  res.on('data', (chunk) => { body += "" + chunk; })
+  res.on('error', (err) => console.error('Ошибка:', err))
+  res.on('end', () => { console.log('Ответ:', body) })
+  res.on('close', () => { console.log('Соединение закрыто') })
+})
+
+request.end(data);
