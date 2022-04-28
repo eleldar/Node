d42cf5df36758ca740cc6b342cb78ea23f0ea074
eldar <eleldar@mail.ru>
2022-04-28 15:12:54 +0300

    Local Library: create genre and author forms (with author death)

--- locallibrary/controllers/authorController.js
+++ locallibrary/controllers/authorController.js
@@ -2,6 +2,7 @@ var Author = require('../models/author');
 var async = require('async');
 var Book = require('../models/book');
 
+const { body,validationResult } = require("express-validator");
 
 // Показать список всех авторов.
 exports.author_list = function(req, res, next) {
@@ -41,14 +42,54 @@ exports.author_detail = function(req, res, next) {
 };
 
 // Показать форму создания автора по запросу GET.
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = function (req, res, next) {
+    res.render('author_form', { title: 'Create Author' });
 };
 
 // Создать автора по запросу POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    // Validate and sanitize fields.
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create Author object with escaped and trimmed data
+        var author = new Author(
+            {
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death,
+            }
+        );
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: author, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Save author.
+            author.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
 // Показать форму удаления автора по запросу GET.
 exports.author_delete_get = function(req, res) {
